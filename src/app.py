79292080f2065303a252a63aac6f0d66e61c704b
KrzysMur <murek.krzys@gmail.com>
2024-01-04 08:02:36 +0100

    Project saving backend

--- src/app.py
+++ src/app.py
@@ -7,6 +7,8 @@ class App:
         sg.theme(theme)
         self.layout = self.create_input_column()
         self.window = sg.Window('ChordSheetWriter', self.layout, resizable=True)
+        self.project_path = None
+        self.project_name = None
 
     @staticmethod
     def create_input_column():
@@ -28,13 +30,26 @@ class App:
                 case "RENDER":
                     print(values["INPUT"])
                 case "SAVE":
-                    save_window = SaveWindow()
-                    save_window.run()
+                    if self.project_path is not None and os.path.exists(self.project_path):
+                        # TODO: saving after location change raises filenotfound error
+                        # Project has been saved before and location didn`t change
+                        self.save(values["INPUT"])
+                    else:
+                        # Saving project for the first time or after location change
+                        save_window = SaveWindow(values["INPUT"])
+                        self.project_path, self.project_name = save_window.run()
         self.window.close()
+
+    def save(self, content):
+        with open(os.path.join(self.project_path, self.project_name, self.project_name + ".txt"), "w") as file:
+            file.write(content)
+        with open(os.path.join(self.project_path, self.project_name, self.project_name + ".tex"), "w") as file:
+            file.write(content)
 
 
 class SaveWindow:
-    def __init__(self):
+    def __init__(self, input_content):
+        self.input_content = input_content
         self.layout = [self.create_path_input(),
                        self.create_project_name_input(),
                        self.create_buttons()]
@@ -56,7 +71,15 @@ class SaveWindow:
         return [sg.Button("Save"), sg.Button("Cancel")]
 
     def save_project(self, path, name):
-        print(os.path.normpath(os.path.join(path, name)))
+        try:
+            os.makedirs(os.path.normpath(os.path.join(path, name)))
+        except FileExistsError:
+            sg.popup("Project directory already exists", title="ERROR")
+        else:
+            with open(os.path.normpath(os.path.join(path, name, name + ".txt")), "w") as file:
+                file.write(self.input_content)
+            with open(os.path.normpath(os.path.join(path, name, name + ".tex")), "w") as file:
+                file.write(self.input_content)
 
     def run(self):
         while True:
@@ -66,7 +89,8 @@ class SaveWindow:
                     break
                 case "Save" | "\r":
                     self.save_project(values["PATH"], values["PROJECTNAME"])
-                    break
+                    self.window.close()
+                    return values["PATH"], values["PROJECTNAME"]
         self.window.close()
 
 
