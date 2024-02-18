b00857067d183721e1c0a3072a92e9aeb73a8338
KrzysMur <murek.krzys@gmail.com>
2024-02-18 18:24:46 +0100

    Besic GUI in PyQt6 (no functionality); icons folder

--- src/app.py
+++ src/app.py
@@ -1,48 +1,77 @@
-import PySimpleGUI as sg
 import os
+import sys
 from tex_generator import TexGenerator
 from input_parser import InputParser
 from config_provider import config
-import subprocess
-from pdflatex import PDFLaTeX
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import Qt
 
 
-class App:
-    def __init__(self, theme='DarkGrey4'):
-        sg.theme(theme)
-        self.layout = self.create_input_column()
-        self.window = sg.Window('ChordSheetWriter', self.layout, resizable=True)
-        self.project_path = None
-        self.project_name = None
+def create_separator():
+    separator = QFrame()
+    separator.setFrameShape(QFrame.Shape.VLine)
+    return separator
 
-    @staticmethod
-    def create_input_column():
-        column = [[sg.Button("RENDER"), sg.Button("SAVE"), sg.Button("OPEN")],
-                  [sg.Multiline(size=(50, 25),
-                                key="INPUT",
-                                expand_x=True,
-                                expand_y=True)]]
-        return column
 
-    def run(self):
-        while True:
-            event, values = self.window.read()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-            match event:
-                case sg.WINDOW_CLOSED:
-                    break
-                case "RENDER":
-                    print(values["INPUT"])
-                case "SAVE":
-                    pass
-                case "OPEN":
-                    pass
+        self.setWindowTitle("ChordSheetWriter")
+        self.setMinimumSize(300, 350)
 
-        self.window.close()
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        self.layout = QVBoxLayout(central_widget)
+        central_widget.setLayout(self.layout)
+
+        self.tool_bar = QHBoxLayout()
+        self.tool_bar.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+        button_size = int(config.get("gui", "toolbar_button_size"))
+
+        self.save_button = QPushButton(QIcon("../resources/icons/save.png"), "", self)
+        self.save_button.setFixedSize(button_size, button_size)
+        self.tool_bar.addWidget(self.save_button)
+
+        self.start_button = QPushButton(QIcon("../resources/icons/start.png"), "", self)
+        self.start_button.setFixedSize(button_size, button_size)
+        self.tool_bar.addWidget(self.start_button)
+
+        self.tool_bar.addWidget(create_separator())
+
+        self.undo_button = QPushButton(QIcon("../resources/icons/undo.png"), "", self)
+        self.undo_button.setFixedSize(button_size, button_size)
+        self.tool_bar.addWidget(self.undo_button)
+
+        self.redo_button = QPushButton(QIcon("../resources/icons/redo.png"), "", self)
+        self.redo_button.setFixedSize(button_size, button_size)
+        self.tool_bar.addWidget(self.redo_button)
+
+        self.tool_bar.addWidget(create_separator())
+
+        self.settings_button = QPushButton(QIcon("../resources/icons/settings.png"), "", self)
+        self.settings_button.setFixedSize(button_size, button_size)
+        self.tool_bar.addWidget(self.settings_button)
+
+        self.layout.addLayout(self.tool_bar)
+
+        self.text_input = QTextEdit()
+        self.layout.addWidget(self.text_input)
+
+        self.show()
+
 
 
 if __name__ == '__main__':
+
     project_name = "project1"
+
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    sys.exit(app.exec())
 
     with open(f"../example_inputs/{project_name}.chordsheet") as file:
         input_content = [line.strip() for line in file.readlines() if line != "\n"]
